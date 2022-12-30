0d29b970b993851a58ffca5068263def8749e689
Johnathan Vail <jvail@newts.org>
2022-12-30 18:22:26 -0500

    add my mods and keys, yeet home row mods

--- keyboards/tominabox1/qaz/keymaps/default/keymap.c
+++ keyboards/tominabox1/qaz/keymaps/default/keymap.c
@@ -13,11 +13,20 @@ enum combo_events {
   COMBO_DEL,
 };
 
+
 #define KC_NUM_SPC LT(_NUM_SYM, KC_SPC)
+
+// jv unused home row mods
 #define KC_GA LGUI_T(KC_A)
 #define KC_AS LALT_T(KC_S)
 #define KC_CD LCTL_T(KC_D)
 #define KC_SF LSFT_T(KC_F)
+
+// jv add left and right hold/tap mods
+#define KC_SZ LSFT_T(KC_Z)
+#define KC_SDOT RSFT_T(KC_DOT)
+
+// jv unused home row mods
 #define KC_SJ RSFT_T(KC_J)
 #define KC_CK RCTL_T(KC_K)
 #define KC_AL RALT_T(KC_L)
@@ -26,8 +35,8 @@ enum combo_events {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_split_space(
     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,    KC_I,   KC_O,     KC_P,
-    KC_GA,  KC_AS,  KC_CD,  KC_SF,   KC_G,   KC_H,  KC_SJ,   KC_CK,  KC_AL, KC_ENT,
-    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M, KC_COMM, KC_DOT,
+    KC_A,  KC_S,  KC_D,  KC_F,   KC_G,   KC_H,  KC_J,   KC_K,  KC_L, KC_ENT,
+    KC_SZ,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M, KC_COMM, KC_SDOT,
     KC_LCTL, KC_LALT, KC_LGUI, KC_BSPC, LT(_NUM_SYM,KC_SPACE), KC_RALT, KC_RGUI, KC_RCTL
   ),
 
@@ -35,15 +44,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
     KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_EQUAL, KC_MINS,
     KC_LCBR, KC_LBRC, KC_LPRN, KC_UNDS, KC_RPRN, KC_RBRC, KC_RCBR, KC_SCLN, KC_QUOTE,
-    RGB_TOG, RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   )
 };
 
 #ifdef COMBO_ENABLE
 const uint16_t PROGMEM combo_bspc[] = {KC_O, KC_P, COMBO_END};
 const uint16_t PROGMEM combo_numbak[] = {KC_0, KC_9, COMBO_END};
-const uint16_t PROGMEM combo_tab[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM combo_esc[] = {KC_E, KC_W, COMBO_END};
+
+const uint16_t PROGMEM combo_esc[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM combo_tab[] = {KC_A, KC_S, COMBO_END};
+
 const uint16_t PROGMEM combo_del[] = {KC_MINS, KC_EQL, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
